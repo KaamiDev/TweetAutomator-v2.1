feb88d08fee0e4a7df8fd1cf19eb495891d8369e
Kaami Dev <kaami@kaamidev.com>
2020-06-11 23:11:31 -0400

    styled header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div>
-			<p>Header</p>
+		<div id="header">
+			<h1 id="header-title">
+				TweetAutomator <span>v2.1</span>
+			</h1>
+			<h3 id="header-subtitle">Twitter Botting Panel</h3>
 		</div>
 	);
 };
