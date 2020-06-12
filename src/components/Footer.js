dc78cbabb08ab299edd3a3636db646d750a8103b
Kaami Dev <kaami@kaamidev.com>
2020-06-12 00:26:44 -0400

    styled footer and implemented dynamic date

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 const Footer = () => {
+	useEffect(() => {
+		let d = new Date();
+		document.getElementById('footer-year').innerHTML = d.getFullYear();
+	}, []);
+
 	return (
-		<div>
-			<p>Footer</p>
+		<div id="footer">
+			<p id="footer-text">
+				&copy; <span id="footer-year">2020</span> TweetAutomator by{' '}
+				<a className="footer-link" href="https://kaamidev.com" target="_blank">
+					Kaami
+				</a>
+			</p>
 		</div>
 	);
 };
