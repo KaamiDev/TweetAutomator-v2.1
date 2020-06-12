86cdc42e1ee07d19481289085bb7db04b454c17d
Kaami Dev <kaami@kaamidev.com>
2020-06-12 00:05:24 -0400

    Created & styled login card component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Login = () => {
 	return (
-		<div>
-			<p>Login</p>
+		<div className="content-section">
+			<div className="login-card">
+				<h4 className="card-title">Authenticate Below.</h4>
+				<form>
+					<input type="text" placeholder="Username" autoComplete="off" id="username" />
+					<input type="password" placeholder="Password" id="password" />
+					<button type="submit" className="login-btn">
+						Login
+					</button>
+				</form>
+			</div>
 		</div>
 	);
 };
