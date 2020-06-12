050933458597ef7f70e4ca78cc5ab0ff116dce71
Kaami Dev <kaami@kaamidev.com>
2020-06-12 00:37:41 -0400

    clicking header now refreshes app

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,9 @@ const Header = () => {
 	return (
 		<div id="header">
 			<h1 id="header-title">
-				TweetAutomator <span>v2.1</span>
+				<a href="/">
+					TweetAutomator <span>v2.1</span>
+				</a>
 			</h1>
 			<h3 id="header-subtitle">Twitter Botting Panel</h3>
 		</div>
