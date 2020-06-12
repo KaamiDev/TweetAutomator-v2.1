3e3fbe7b2c8087bbb5964be427bf90855aba0a45
Kaami Dev <kaami@kaamidev.com>
2020-06-12 00:29:53 -0400

    created temp submit function redirecting to home root

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const Login = () => {
+	let history = useHistory();
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		history.push('/');
+	};
+
 	return (
 		<div className="content-section">
 			<div className="login-card">
 				<h4 className="card-title">Authenticate Below.</h4>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<input type="text" placeholder="Username" autoComplete="off" id="username" />
 					<input type="password" placeholder="Password" id="password" />
 					<button type="submit" className="login-btn">
