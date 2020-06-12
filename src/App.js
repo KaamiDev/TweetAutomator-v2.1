1862008756777c5b325bdc72f4bb555ea9b7c524
Kaami Dev <kaami@kaamidev.com>
2020-06-11 22:46:24 -0400

    added header and footer to routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
-
 import Panel from './components/Panel';
 import Login from './components/Login';
 import Admin from './components/Admin';
+import Header from './components/Header';
+import Footer from './components/Footer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 
@@ -10,11 +11,13 @@ function App() {
 	return (
 		<div>
 			<Router>
+				<Header />
 				<Switch>
 					<Route exact path="/" component={Panel} />
-					<Route exact path="/login" component={Login} />
-					<Route exact path="/admin" component={Admin} />
+					<Route path="/login" component={Login} />
+					<Route path="/admin" component={Admin} />
 				</Switch>
+				<Footer />
 			</Router>
 		</div>
 	);
