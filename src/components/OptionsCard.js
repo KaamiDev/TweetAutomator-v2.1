7117d9ee7d7c82a2079dbfc0b91ac1a7c750e697
Kaami Dev <kaami@kaamidev.com>
2020-06-13 02:10:40 -0400

    minor functionality to option buttons

--- src/components/OptionsCard.js
+++ src/components/OptionsCard.js
@@ -1,19 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const OptionsCard = () => {
+	const [ selectedOption, setSelectedOption ] = useState('RT');
+
+	const handleClick = (e) => {
+		setSelectedOption(e.target.innerHTML);
+	};
+
 	return (
 		<div className="options-card">
 			<h4 className="card-title">Manage Twitter Options</h4>
 			<div className="option-btn-containers">
-				<button className="option-btn active">RT</button>
-				<button className="option-btn">Like</button>
-				<button className="option-btn">Follow</button>
-				<button className="option-btn">Report</button>
+				<button onClick={handleClick} className={`option-btn ${selectedOption === 'RT' ? 'active' : ''}`}>
+					RT
+				</button>
+				<button onClick={handleClick} className={`option-btn ${selectedOption === 'Like' ? 'active' : ''}`}>
+					Like
+				</button>
+				<button onClick={handleClick} className={`option-btn ${selectedOption === 'Follow' ? 'active' : ''}`}>
+					Follow
+				</button>
+				<button onClick={handleClick} className={`option-btn ${selectedOption === 'Report' ? 'active' : ''}`}>
+					Report
+				</button>
 			</div>
 			<input type="number" placeholder="# of Bots (max: 4)" min="1" max="4" autoComplete="off" id="username" />
-			<input type="text" placeholder="Tweet Link" autoComplete="off" id="password" />
+			<input
+				type="text"
+				placeholder={selectedOption === 'Like' || selectedOption === 'RT' ? 'Tweet Link' : 'Account @'}
+				autoComplete="off"
+				id="password"
+			/>
 			<button type="submit" className="login-btn">
-				RT Tweet
+				{selectedOption} {selectedOption === 'RT' || selectedOption === 'Like' ? 'Tweet' : 'Account'}
 			</button>
 		</div>
 	);
