352829d3c5944d2b997b1ba17b7abfc8b5963b45
Kaami Dev <kaami@kaamidev.com>
2020-06-12 21:58:46 -0400

    created and styled twitter options card component

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -140,7 +140,25 @@ const Panel = () => {
 					</div>
 				</div>
 				<div className="options-card">
-					<p>Hello</p>
+					<h4 className="card-title">Manage Twitter Options</h4>
+					<div className="option-btn-containers">
+						<button className="option-btn active">RT</button>
+						<button className="option-btn">Like</button>
+						<button className="option-btn">Follow</button>
+						<button className="option-btn">Report</button>
+					</div>
+					<input
+						type="number"
+						placeholder="# of Bots (max: 4)"
+						min="1"
+						max="4"
+						autoComplete="off"
+						id="username"
+					/>
+					<input type="text" placeholder="Tweet Link" autoComplete="off" id="password" />
+					<button type="submit" className="login-btn">
+						RT Tweet
+					</button>
 				</div>
 			</div>
 		</div>
