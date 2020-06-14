c4a3f3471c91f85ed893a22933bfd263281888f9
Kaami Dev <kaami@kaamidev.com>
2020-06-14 19:29:48 -0400

    setup proper routing

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+app.use('/panel', require('./routes/panel'));
+app.use('/login', require('./routes/login'));
+app.use('/admin', require('./routes/admin'));
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
 	console.log('Server started and listeing on port ' + PORT);
