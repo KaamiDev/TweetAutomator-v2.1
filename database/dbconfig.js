b57408c737e70c0805c11ddd4fffdac03bb7b206
Kaami Dev <kaami@kaamidev.com>
2020-06-15 22:27:32 -0400

    dbconfig autocreates admin acc if one doesn't exist

--- database/dbconfig.js
+++ database/dbconfig.js
@@ -1,4 +1,13 @@
 var db = require('diskdb');
 db = db.connect('./database/DB', [ 'accounts', 'users' ]);
 
+if (!db.users.findOne({ username: 'admin' })) {
+	let user = {
+		username: 'admin',
+		password: 'admin'
+	};
+	db.users.save(user);
+	console.log('Admin account created.');
+}
+
 module.exports = db;
