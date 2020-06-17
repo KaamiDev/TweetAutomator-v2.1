9610351d7a7c0edae6292c7046c72c10b0fbd470
Kaami Dev <kaami@kaamidev.com>
2020-06-16 23:16:53 -0400

    added cors module for cross origin requests

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
