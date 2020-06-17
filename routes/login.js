389997cd15e6dba95fe8abcd2016e2d4f9125ea7
Kaami Dev <kaami@kaamidev.com>
2020-06-16 23:46:23 -0400

    Implemented login route that should work (hopefully)

--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,20 @@
 const express = require('express');
+const db = require('diskdb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
 	res.send('Login');
+});
+
+router.post('/', (req, res) => {
+	if (req.body.username && req.body.password) {
+		let user = db.users.findOne({ username: req.body.username.toLowerCase(), password: req.body.password });
+		if (user) {
+			res.status(200).send(user._id);
+		} else {
+			res.status(200).send('Invalid Credentials.');
+		}
+	}
 });
 
 module.exports = router;
