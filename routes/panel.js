7261dcefe004c85588d96e4d51436c841442633f
Kaami Dev <kaami@kaamidev.com>
2020-06-17 00:30:24 -0400

    removed panel get route

--- routes/panel.js
+++ routes/panel.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-	res.send('Panel');
+router.post('/', (req, res) => {
+	// post route
 });
 
 module.exports = router;
