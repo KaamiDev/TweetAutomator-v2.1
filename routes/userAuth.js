7860a9828278fc5346edfae87621f6408ec02e81
Kaami Dev <kaami@kaamidev.com>
2020-06-27 18:01:37 -0400

    added adminauth route

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,6 +1,12 @@
 const db = require('../database/dbconfig');
 
-module.exports.adminAuth = (req, res, next) => {};
+module.exports.adminAuth = (req, res, next) => {
+	if (req.user.username === 'admin') {
+		next();
+	} else {
+		res.status(401).send('Not Authorized.');
+	}
+};
 
 module.exports.userAuth = (req, res, next) => {
 	let user = db.users.findOne({ _id: req.headers.authtoken });
