d7c4bba49c7d3e7a0f4642d0bdb189611f2a364b
Kaami Dev <kaami@kaamidev.com>
2020-06-27 18:44:12 -0400

    fixed comfirm pass id error

--- src/components/AdminAddAccess.js
+++ src/components/AdminAddAccess.js
@@ -6,7 +6,7 @@ const AdminAddAccess = () => {
 			<h4 className="card-title">Add Access Login</h4>
 			<input type="text" placeholder="Username" autoComplete="off" id="username" />
 			<input type="password" placeholder="Password" autoComplete="off" id="password" />
-			<input type="password" placeholder="Confirm Password" autoComplete="off" id="password" />
+			<input type="password" placeholder="Confirm Password" autoComplete="off" id="confirm-password" />
 			<button type="submit" className="login-btn">
 				Add Access
 			</button>
