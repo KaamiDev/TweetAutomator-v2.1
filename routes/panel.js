2b8633141a1fad28c58bd7a49f8f4129db1c169d
Kaami Dev <kaami@kaamidev.com>
2020-06-28 19:32:49 -0400

    get route now sends over keys as well

--- routes/panel.js
+++ routes/panel.js
@@ -6,7 +6,11 @@ const router = express.Router();
 router.get('/', userAuth, (req, res) => {
 	let accounts = db.accounts.find();
 	username = req.user.username;
-	res.status(200).send({ accounts, username });
+	keys = {
+		consumerKey: process.env.CONSUMER_KEY,
+		consumerSecret: process.env.CONSUMER_SECRET
+	};
+	res.status(200).send({ accounts, username, keys });
 });
 
 module.exports = router;
